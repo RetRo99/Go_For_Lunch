b79ec68ead1595619bc8ddf36fd869e76946d128
Rok Retar <retar.rok@gmail.com>
2020-01-12 21:32:36 +0100

    moved map from activity to fragment

--- app/src/main/java/com/retar/go4lunch/MainActivity.kt
+++ app/src/main/java/com/retar/go4lunch/MainActivity.kt
@@ -3,41 +3,27 @@ package com.retar.go4lunch
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (permissionsGranted()) loadMap() else requestPermissions()
+        if (permissionsGranted()) loadMapFragment() else requestPermissions()
     }
 
-    private fun loadMap() {
-        val mapFragment = fragmentManager
-            .findFragmentById(R.id.map) as MapFragment
-        mapFragment.getMapAsync(this)
-
+    private fun loadMapFragment() {
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer,MapView.newInstance()).commit()
     }
 
     private fun permissionsGranted(): Boolean {
@@ -56,31 +42,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         )
     }
 
-    private fun getLastLocation() {
-        val fusedLocationClient: FusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(this)
 
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                location?.let {
-                    zoomToLocation(it)
-                }
-            }
-    }
-
-    private fun zoomToLocation(location: Location) {
-        Toast.makeText(this, "bebe", Toast.LENGTH_LONG).show()
-        val currentLatLng = LatLng(location.latitude, location.longitude)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, ZOOM_MODE))
-        map.addMarker(MarkerOptions().position(currentLatLng))
-    }
-
-    override fun onMapReady(map: GoogleMap?) {
-        map?.let {
-            this.map = it
-            getLastLocation()
-        }
-    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -93,7 +55,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 if (grantResults.isNotEmpty()
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED
                 ) {
-                    loadMap()
+                    loadMapFragment()
                 } else {
                     showRationaleForPermissions()
                 }
@@ -103,27 +65,33 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun showRationaleForPermissions() {
         if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            showPermissionsRequiredDialog("Permission to access location is needed for the app to work.", dontAskAgainChecked = false)
-        }else {
-            showPermissionsRequiredDialog("You previously denied location permission. Click ok to open settings to enable it.", dontAskAgainChecked = true)
+            showPermissionsRequiredDialog(
+                "Permission to access location is needed for the app to work.",
+                dontAskAgainChecked = false
+            )
+        } else {
+            showPermissionsRequiredDialog(
+                "You previously denied location permission. Click ok to open settings to enable it.",
+                dontAskAgainChecked = true
+            )
         }
     }
 
-    private fun showPermissionsRequiredDialog(body:String, dontAskAgainChecked:Boolean){
+    private fun showPermissionsRequiredDialog(body: String, dontAskAgainChecked: Boolean) {
         val builder = AlertDialog.Builder(this)
         builder.setMessage(body)
             .setTitle("Permission required")
             .setPositiveButton("OK") { _, _ ->
-               if(!dontAskAgainChecked) requestPermissions()    else openSettings()
+                if (!dontAskAgainChecked) requestPermissions() else openSettings()
             }
-            .setNegativeButton("EXIT") {_,_ ->
+            .setNegativeButton("EXIT") { _, _ ->
                 finish()
             }
             .create()
             .show()
     }
 
-    private fun openSettings(){
+    private fun openSettings() {
         val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
         intent.data = Uri.parse("package:$packageName")
         startActivity(intent)
@@ -133,5 +101,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         const val LOCATION_REQUEST_CODE = 15
         const val ZOOM_MODE = 15f
     }
-    
+
 }
